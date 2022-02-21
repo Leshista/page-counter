638a4dce9a7de97606a8eb78e8640e81461d5e3a
leshista <a.leshista@gmail.com>
2022-02-21 21:47:14 +0300

    Now submit return an object to array of state

--- src/App.js
+++ src/App.js
@@ -6,17 +6,29 @@ import './main.css';
 
 function App() {
     const [formIsOpen, setFormIsOpen] = useState(false);
+    const [items, setItems] = useState([]);
+    const [itemId, setItemId] = useState(0);
 
     const formOpenHandler = () => {
         setFormIsOpen(!formIsOpen);
     };
     const SubmitHandler = (event) => {
         event.preventDefault();
+        setItems((prevItems) => [
+            ...prevItems,
+            {
+                book: event.target[0].value,
+                pages: event.target[1].value,
+                id: itemId,
+                key: itemId,
+            },
+        ]);
+        setItemId(itemId + 1);
         setFormIsOpen(!formIsOpen);
     };
 
     return (
-        <main className='main'>
+        <main className="main">
             {formIsOpen ? (
                 <Form onAddNote={SubmitHandler} />
             ) : (
